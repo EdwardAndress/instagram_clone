a0bc5123a07e695dc91b94e3f0a03fea657b68b9
Edward J. Andress <eddie_andress@hotmail.com>
2014-08-18 20:45:22 +0100

    initial commit

---
+++ instagram/spec/features/post_feature_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe 'posts' do 
+
+	context 'no posts' do
+
+		it 'shows a message to indicate a lack of posts' do
+			visit '/posts'
+			expect(page).to have_content('No posts yet')
+		end
+
+	end
+
+	context 'with posts' do
+
+		before(:each) {Post.create(title: 'This is the first post')}
+
+		it 'displays a list of posts' do
+			visit '/posts'
+			expect(page).to have_content('This is the first post')
+		end
+
+	end
+
+	context 'creating posts' do 
+
+		it 'adds the posts from a form' do 
+			visit '/posts'
+			click_link 'New post'
+			fill_in 'Title', with: 'Another post'
+			click_button 'Create post'
+			expect(page).to have_content 'Another post'
+			expect(current_path).to eq '/posts'
+		end
+
+		it 'can attach an image to a post' do 
+			visit '/posts'
+			click_link 'New post'
+			fill_in 'Title', with: 'Another another post'
+			attach_file 'Picture', Rails.root.join('spec/images/BonjourViolaine.jpg')
+			click_button 'Create post'
+
+			expect(page).to have_css('img.uploaded_image')
+		end
+
+	end
+
+end
